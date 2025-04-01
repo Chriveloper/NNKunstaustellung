9860cb0ba19aa98d06c7f1b0d0da9ab635adb37e
Chriveloper <111074541+Chriveloper@users.noreply.github.com>
2025-04-01 11:54:26 +0200

    Refactor main.py to improve room data handling and add initial functions for artwork and shadow calculations

--- main.py
+++ main.py
@@ -5,8 +5,24 @@ from shapely import Point, LineString, Polygon
 from jamico import create_room, plot_room
 room = create_room(30, 30, 0.2, 15)
 
-outer_room = room[2]
-print(outer_room)
+raumPolygon = room[2]
+kunstwerkPunkte = room[1]
+waendeLinien = room[0]
+
+print("Kunstausstellung:", kunstausstellung)
+print("Kunstwerke:", kunstwerke)
+# print("Wände:", waende)
+
+
+def kunstwerkPolygon(kunstwerk):
+    sichtPolygon = raumPolygon
+    kunstwerkPunkt = kunstwerk
+    for wand in waendeLinien:
+
+def schatten(punkt, wand):
+    linie1 = LineString([punkt, wand.coords[0]])
+    linie2 = LineString([punkt, wand.coords[1]])
+
 
 guards = [Point(random.random() * 15, random.random() * 15) for i in range(4)]
 plot_room(room, guards)
